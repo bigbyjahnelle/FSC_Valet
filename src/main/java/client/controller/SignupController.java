5f0fbbbe5e078b053a766f469fbd28d7210c4b09
Adrianna Lambert <153338615+lynn8exe@users.noreply.github.com>
2026-04-06 12:15:14 -0400

    minor styling and error fixes

---
+++ src/main/java/client/controller/SignupController.java
@@ -0,0 +1,11 @@
+package client.controller;
+
+import javafx.fxml.FXML;
+
+public class SignupController {
+
+    @FXML
+    public void initialize() {
+        System.out.println("Signup screen loaded");
+    }
+}
