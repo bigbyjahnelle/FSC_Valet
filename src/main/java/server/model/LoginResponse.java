26ccad6162a25068eff4470df2eebdf50ac378db
Cobin <halfkey.studio.games@gmail.com>
2026-04-17 14:59:09 -0400

    feat: Added fullName var in LoginResponse
    feat: Created get/set FullName in LoginResponse

--- src/main/java/server/model/LoginResponse.java
+++ src/main/java/server/model/LoginResponse.java
@@ -4,6 +4,7 @@ public class LoginResponse {
     private boolean success;
     private String message;
     private String token;
+    private String fullName;
 
     public boolean isSuccess() { return success; }
     public void setSuccess(boolean success) { this.success = success; }
@@ -13,4 +14,7 @@ public class LoginResponse {
 
     public String getToken() { return token; }
     public void setToken(String token) { this.token = token; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
 }
