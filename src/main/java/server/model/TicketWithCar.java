bfda7a24632ba9f0644142d14ab8621484ffd3fb
Cobin <halfkey.studio.games@gmail.com>
2026-04-30 22:49:21 -0400

    feat: Created default and two parameter constructor for TicketWithCar

--- src/main/java/server/model/TicketWithCar.java
+++ src/main/java/server/model/TicketWithCar.java
@@ -21,4 +21,29 @@ public class TicketWithCar
     private int year;
     private String color;
     private String licensePlate;
+
+    public TicketWithCar() {}
+
+    public TicketWithCar(Ticket ticket, Car car)
+    {
+        this.ticketId     = ticket.getTicketId();
+        this.ticketNumber = ticket.getTicketNumber();
+        this.customerId   = ticket.getCustomerId();
+        this.carId        = ticket.getCarId();
+        this.type         = ticket.getType();
+        this.status       = ticket.getStatus();
+        this.archived     = ticket.isArchived();
+        this.notes        = ticket.getNotes();
+        this.createdAt    = ticket.getCreatedAt();
+        this.completedAt  = ticket.getCompletedAt();
+
+        if (car != null)
+        {
+            this.make         = car.getMake();
+            this.model        = car.getModel();
+            this.year         = car.getYear();
+            this.color        = car.getColor();
+            this.licensePlate = car.getLicensePlate();
+        }
+    }
 }
